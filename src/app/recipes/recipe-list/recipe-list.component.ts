1b6001cba082c92484c5174fb0e664575a8fbd6d
dario-nihil <dario.nihil@gmail.com>
2022-02-02 18:00:53 +0100

    implemented basic components layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply test',
+      'https://www.inspiredtaste.net/wp-content/uploads/2022/01/Homemade-Vegan-Meatballs-1-1200.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply test',
+      'https://www.inspiredtaste.net/wp-content/uploads/2022/01/Homemade-Vegan-Meatballs-1-1200.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
